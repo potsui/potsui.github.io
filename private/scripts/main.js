b833e4b1df2eddca123abf595a4b35cc9035a138
Po Tsui <potsui@stanford.edu>
2017-12-19 22:27:37 -0800

    Change close icon to use hamburger parts

--- private/scripts/main.js
+++ private/scripts/main.js
@@ -2,6 +2,5 @@ var $ = jQuery;
 
 function toggleNav() {
     $('.navpage').toggleClass('hidden');
-    $('.navpage-toggle .hamburger').toggleClass('hidden');
-    $('.navpage-toggle .close').toggleClass('hidden');
+    $('.navpage-toggle .navpage-toggle-icon').toggleClass('close');
 }
