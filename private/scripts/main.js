829e5838068d470bed7d96f0fd80d7b73fda4dbd
Po Tsui <potsui@stanford.edu>
2017-12-19 16:59:24 -0800

    Add basic navpage and about page

---
+++ private/scripts/main.js
@@ -0,0 +1,7 @@
+var $ = jQuery;
+
+function toggleNav() {
+    $('.navpage').toggleClass('hidden');
+    $('.navpage-toggle .hamburger').toggleClass('hidden');
+    $('.navpage-toggle .close').toggleClass('hidden');
+}
