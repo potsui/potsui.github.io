d327ea189e87922e3cc1657346076c491c04b3d6
Po Tsui <potsui@stanford.edu>
2017-12-19 23:52:34 -0800

    Load photography page

--- private/scripts/main.js
+++ private/scripts/main.js
@@ -1,6 +1,38 @@
 var $ = jQuery;
 
-function toggleNav() {
-    $('.navpage').toggleClass('hidden');
-    $('.navpage-toggle .navpage-toggle-icon').toggleClass('close');
-}
+var main = (function(){
+    var photography = {
+        20171214: [
+            "IMG_2771",
+            "IMG_2780",
+            "IMG_2782",
+            "IMG_2823",
+            "IMG_2846",
+            "IMG_2856",
+        ],
+    };
+
+    var files = {
+        photography: photography,
+    };
+
+    function toggleNav() {
+        $('.navpage').toggleClass('hidden');
+        $('.navpage-toggle .navpage-toggle-icon').toggleClass('close');
+    }
+
+    function loadFolder(folder) {
+        var list = files.photography[folder];
+        $('#photography-content').empty();
+        $('#photography-content').append('<div class="row" id="' + folder + '"></div>');
+        for (var i in list) {
+            console.log(list[i]);
+            $('#' + folder).append('<div class="col-sm-8 col-md-8"><img src="images/photography/' + folder + '/' + list[i] +'.jpg" class="img-responsive"></div>');
+        }
+    }
+
+    return {
+        toggleNav: toggleNav,
+        loadFolder: loadFolder,
+    };
+})();
