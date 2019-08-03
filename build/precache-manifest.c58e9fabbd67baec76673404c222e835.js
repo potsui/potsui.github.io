3f85b32f69c6d53bf51bf84b9a08c99755bb0b05
Po Tsui <potsui@stanford.edu>
2019-08-02 20:49:30 -0700

    Change to React barebones template

---
+++ build/precache-manifest.c58e9fabbd67baec76673404c222e835.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6505b72e6bd18d8b71c43009cd769fc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "866328652b5513838c13",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "866328652b5513838c13",
+    "url": "/static/js/main.140ae15e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
